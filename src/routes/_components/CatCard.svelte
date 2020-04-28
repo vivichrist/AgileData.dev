f0d2f6601e852b0e1bb715445f853b3787a4ec8e
Vivian Stewart <vivichrist@gmail.com>
2020-04-28 12:32:02 +1200

    app/catalog-details

--- src/routes/_components/CatCard.svelte
+++ src/routes/_components/CatCard.svelte
@@ -28,7 +28,7 @@
         on:click={() => filterfn([item.object, ...item.topics])}
         on:contextmenu|preventDefault={handlePopup}>
     <span class="float-right m-0 p-1">
-      <a role="button" class="btn btn-primary btn-sm p-0 m-0 card-icon" href="catalog-details" on:click={() => $pageseg = item}>
+      <a role="button" class="btn btn-primary btn-sm p-0 m-0 card-icon" href="app/catalog-details" on:click={() => $pageseg = item}>
         <svg class="bi bi-pencil m-0 p-0" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" d="M11.293 1.293a1 1 0 011.414 0l2 2a1 1 0 010 1.414l-9 9a1 1 0 01-.39.242l-3 1a1 1 0 01-1.266-1.265l1-3a1 1 0 01.242-.391l9-9zM12 2l2 2-9 9-3 1 1-3 9-9z" clip-rule="evenodd"/>
           <path fill-rule="evenodd" d="M12.146 6.354l-2.5-2.5.708-.708 2.5 2.5-.707.708zM3 10v.5a.5.5 0 00.5.5H4v.5a.5.5 0 00.5.5H5v.5a.5.5 0 00.5.5H6v-1.5a.5.5 0 00-.5-.5H5v-.5a.5.5 0 00-.5-.5H3z" clip-rule="evenodd"/>
